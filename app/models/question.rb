c75079ecb57e5cd34bc0b714493bf750ea8bf79a
Miguel Ricaño <mricanho@gmail.com>
2022-05-09 11:58:52 -0500

    Add nested form stimulus

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,7 @@
 class Question < ApplicationRecord
   belongs_to :quiz
+  has_many :answers, dependent: :destroy
+  accepts_nested_attributes_for :answers, allow_destroy: true
+
+  enum question_type: {single_choice: 0, multiple_choice: 1, text: 2}
 end
