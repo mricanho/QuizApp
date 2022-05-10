f1e8ef5c9fe85468a5a8b4a4aac2d63f5180366d
Miguel Ricaño <mricanho@gmail.com>
2022-05-10 13:07:09 -0500

    Update show view for quizes

--- app/javascript/controllers/dynamic_select_controller.js
+++ app/javascript/controllers/dynamic_select_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["select", "choice", "long"]
+  static targets = ["select", "choice", "text"]
 
   connect() {
     this.selected()
@@ -16,14 +16,14 @@ export default class extends Controller {
       case 'multiple_choice':
         this.choiceTarget.classList.remove('hidden')
         break;
-      case 'long_answer':
-        this.longTarget.classList.remove('hidden')
+      case 'text':
+        this.textTarget.classList.remove('hidden')
         break;
     }
   }
 
   hideFields() {
     this.choiceTarget.classList.add('hidden')
-    this.longTarget.classList.add('hidden')
+    this.textTarget.classList.add('hidden')
   }
 }
